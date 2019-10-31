8ea7a7dc277e7b106124efaa6c20b988d817b426
Anton Lastochkin <alastochkin@example.com>
2019-10-31 17:16:43 +0300

    add stubs for frontend

---
+++ frontend/gui/main.cpp
@@ -0,0 +1,24 @@
+#include "GLogger.h"
+#include <iostream>
+
+//Логгер выдачи в stderr
+class CerrLogger : public core::GLogger
+{
+public:
+	CerrLogger()
+	{
+		//Устанавливаем текущий логгер как активный
+		setCurrentLogger(this);
+	}
+	void log(std::string info, core::LOG_LEVEL lvl)
+	{
+		std::cerr << info;
+	}
+};
+static CerrLogger gCerrLogger;
+
+int main(int argc, char *argv[])
+{
+	std::cout<<"Future gui!";
+	return 0;
+}
